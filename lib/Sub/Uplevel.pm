18cf2338a0b6e72ab14a5fa13c4d7e43eda4d7f0
David Golden <dagolden@cpan.org>
2007-12-12 13:08:39 +0000

    - bumped version to 0.1801 - removed executable bit from example uplevel-demo.pl
    git-svn-id: https://dagolden.googlecode.com/svn/Sub-Uplevel/trunk@1517 dfce27d5-b31c-0410-bb09-030b4413eeba

--- lib/Sub/Uplevel.pm
+++ lib/Sub/Uplevel.pm
@@ -2,7 +2,7 @@ package Sub::Uplevel;
 
 use strict;
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = '0.18';
+$VERSION = '0.1801';
 
 # We must override *CORE::GLOBAL::caller if it hasn't already been 
 # overridden or else Perl won't see our local override later.
